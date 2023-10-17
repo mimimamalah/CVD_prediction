59794a00ecb0c9369f9a905b77a38277c12346e7
antoine.schutz@epfl.ch <antoine.schutz@epfl.ch>
2023-10-17 19:26:32 +0200

    update code comment

--- implementations.py
+++ implementations.py
@@ -38,9 +38,9 @@ def compute_loss(y, tx, w):
     """Calculate the loss using either MSE or MAE.
 
     Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        w: shape=(2,). The vector of model parameters.
+        y: shape=(N,1)
+        tx: shape=(N,D)
+        w: shape=(D,1). The vector of model parameters.
 
     Returns:
         the value of the loss (a scalar), corresponding to the input parameters w.
@@ -53,12 +53,12 @@ def compute_gradient(y, tx, w):
     """Computes the gradient at w.
 
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        w: numpy array of shape=(2, ). The vector of model parameters.
+        y: numpy array of shape=(N,1)
+        tx: numpy array of shape=(N,D)
+        w: numpy array of shape=(D,1). The vector of model parameters.
 
     Returns:
-        An numpy array of shape (2, ) (same shape as w), containing the gradient of the loss at w.
+        An numpy array of shape (D,1) (same shape as w), containing the gradient of the loss at w.
     """
 
     e = y - np.dot(tx, w)
@@ -69,12 +69,12 @@ def compute_stoch_gradient(y, tx, w):
     """Compute a stochastic gradient at w from just few examples n and their corresponding y_n labels.
 
     Args:
-        y: shape=(N, )
-        tx: shape=(N,2)
-        w: shape=(2, ). The vector of model parameters.
+        y: shape=(N,1)
+        tx: shape=(N,D)
+        w: shape=(D,1). The vector of model parameters.
 
     Returns:
-        An array of shape (2, ) (same shape as w), containing the stochastic gradient of the loss at w.
+        An array of shape (D,1) (same shape as w), containing the stochastic gradient of the loss at w.
     """
 
     ### SOLUTION
@@ -83,19 +83,20 @@ def compute_stoch_gradient(y, tx, w):
     return grad, err
 
 
+
 def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """The Gradient Descent (GD) algorithm using Mean Square error Loss.
 
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        y: numpy array of shape=(N,1)
+        tx: numpy array of shape=(N,D)
+        initial_w: numpy array of shape=(D,1). The initial guess (or the initialization) for the model parameters
         max_iters: a scalar denoting the total number of iterations of GD
         gamma: a scalar denoting the stepsize
 
     Returns:
         losses: a list of length max_iters containing the loss value (scalar) for each iteration of GD
-        w: The lastparameter w of shape (2, ),
+        w: The lastparameter w of shape (D,1),
     """
 
     w = initial_w
@@ -115,9 +116,10 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     By default , Batch_size is 1
 
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+
+        y: numpy array of shape=(N,1)
+        tx: numpy array of shape=(N,D)
+        initial_w: numpy array of shape=(D,1). The initial guess (or the initialization) for the model parameters
         max_iters: a scalar denoting the total number of iterations of GD
         gamma: a scalar denoting the stepsize
 
@@ -166,12 +168,12 @@ def ridge_regression(y, tx, lambda_):
     """implement ridge regression.
 
     Args:
-        y: numpy array of shape (N,), N is the number of samples.
+        y: numpy array of shape (N,1), N is the number of samples.
         tx: numpy array of shape (N,D), D is the number of features.
         lambda_:scalar :  regularisation parameter
 
     Returns:
-        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        w: optimal weights, numpy array of shape (D,1), D is the number of features.
         loss : scalar
     """
     aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
@@ -195,13 +197,13 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
         y:  shape=(N, 1)
         tx: shape=(N, D)
         lambda_:scalar :  regularisation parameter
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        initial_w: numpy array of shape=(D,1). The initial guess (or the initialization) for the model parameters
         max_iters: a scalar denoting the total number of iterations of GD
         gamma: a scalar denoting the stepsize
 
     Returns:
-        loss: scalar number
         w: shape=(D, 1)
+        loss: scalar number
 
     """
 
@@ -219,13 +221,13 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     Args:
         y:  shape=(N, 1)
         tx: shape=(N, D)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
+        initial_w: numpy array of shape=(D,1). The initial guess (or the initialization) for the model parameters
         max_iters: a scalar denoting the total number of iterations of GD
         gamma: a scalar denoting the stepsize
 
     Returns:
-        loss: scalar number
         w: shape=(D, 1)
+        loss: scalar number
 
     """
     return Logistics.logistic_regression(y, tx, initial_w, max_iters, gamma)
